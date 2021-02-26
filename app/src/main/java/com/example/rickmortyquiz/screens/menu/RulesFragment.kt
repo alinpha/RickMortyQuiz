3bf395ef42d7d37c63c5218583607bdbff4f34c1
Aline Vetrov <alinpha@gmail.com>
2021-02-26 18:46:43 -0400

    about and rules pages completed

--- app/src/main/java/com/example/rickmortyquiz/screens/menu/RulesFragment.kt
+++ app/src/main/java/com/example/rickmortyquiz/screens/menu/RulesFragment.kt
@@ -1,10 +1,11 @@
-package com.example.rickmortyquiz
+package com.example.rickmortyquiz.screens.menu
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.rickmortyquiz.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
